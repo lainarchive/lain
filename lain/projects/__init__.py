68a80666394a7f1a4c584743100250a4526e58e4
lain <lixkirb@gmail.com>
2026-09-17 14:00:39 -0400

    Expose project discovery API

--- lain/projects/__init__.py
+++ lain/projects/__init__.py
@@ -1,1 +1,5 @@
 """Project discovery and management."""
+
+from .discovery import Project, discover, load, project_roots, registry_path, save
+
+__all__ = ["Project", "discover", "load", "project_roots", "registry_path", "save"]
